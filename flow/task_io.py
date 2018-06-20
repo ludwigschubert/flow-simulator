216ad26e4284d5c40e17c1d81d7b800a8318ef50
Ludwig Schubert <github-email@ludwigschubert.de>
2018-06-19 17:23:10 -0700

    fix relative path issue

--- flow/task_io.py
+++ flow/task_io.py
@@ -3,16 +3,20 @@ import logging
 
 from flow.io_adapter import io
 from flow.path_template import PathTemplate
+from flow.path import AbsolutePath
 from lucid.misc.io import load as lucid_io_load
 
+
 from absl import flags
+
 FLAGS = flags.FLAGS
 
-def load(raw_path: str, transform: str = 'None') -> Sequence:
-  assert raw_path.startswith('/')
-  path = PathTemplate.path_template_prefix + raw_path
-  with io.reading(path) as handle:
-    result = lucid_io_load(handle)
-  if transform == 'lines':
-    result = result.split("\n")
-  return result
+
+def load(path: str, transform: str = "None") -> Sequence:
+    assert path.startswith("/")
+    # path = PathTemplate.path_template_prefix + raw_path # TODO: rethink
+    with io.reading(AbsolutePath(path)) as handle:
+        result = lucid_io_load(handle)
+    if transform == "lines":
+        result = result.split("\n")
+    return result
